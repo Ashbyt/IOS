dad89ea04646d4b01f5fa902983b8927894625b9
Ashley B <abythell@Hotmail.com>
2024-02-07 18:58:07 +1100

    Initial Commit

---
+++ ashtest3github/ashtest3githubApp.swift
@@ -0,0 +1,18 @@
+//
+//  ashtest3githubApp.swift
+//  ashtest3github
+//
+//  Created by Ashley Bythell on 7/2/2024.
+//
+///tesr
+///test3
+import SwiftUI
+
+@main
+struct ashtest3githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
