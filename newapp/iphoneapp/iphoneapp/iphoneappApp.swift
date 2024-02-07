924048fc80f2450c088597854cd2862c7f8f8588
Ashley Bythell <abythell@Hotmail.com>
2024-02-07 18:42:26 +1100

    Add files via upload

---
+++ newapp/iphoneapp/iphoneapp/iphoneappApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphoneappApp.swift
+//  iphoneapp
+//
+//  Created by Ashley Bythell on 7/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct iphoneappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
